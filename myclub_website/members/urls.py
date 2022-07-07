41fedd2ba6c25f177d97accf79cd38d5a1ddc0cb
Himanshu-shrma <51356608+Himanshu-shrma@users.noreply.github.com>
2022-07-07 20:04:53 +0530

    Delete myclub_website directory

--- myclub_website/members/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path,include
-from . import views
-
-urlpatterns = [
-    
-    path('login-user',views.login_user,name='login-user'),
-    path('logout-user',views.logout_user,name='logout-user'),
-    path('register-user',views.register_user,name='register-user'),
-]
